551874373d6f58c913c265d7d989042d5407c82a
Shradhaa JR <jshradhaa@gmail.com>
2018-02-15 02:32:26 +0530

    Create CloneLL.java

---
+++ SHRADHAA J/LinkedList/CloneLL.java
@@ -0,0 +1,45 @@
+package linkedlist;
+
+import java.util.*;
+
+public class CloneLL {
+
+	public static void main(String[] args) {
+		Node head=new Node(1, null, null);
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter size of linked list: ");
+		int n=sc.nextInt();
+		
+		Node temp=head;
+		for(int i=2;i<=n;i++)
+		{
+			Node nxt=new Node(i, null, null);
+			temp.next=nxt;
+			temp=temp.next;
+		}
+		
+		System.out.println("Enter random links: ");
+		temp=head;
+		for(int i=1;i<=n;i++)
+		{
+			System.out.print("Random link for "+i+": ");
+			int r=sc.nextInt();
+			
+			Node f=head.find(head, r);
+			//System.out.println();
+			if(f!=null)
+			{
+				temp.random=f;
+			}
+			temp=temp.next;
+		}
+		
+		head.print(head);
+		
+		Node cl=head.clone(head);
+		cl.print(cl);
+
+	}
+
+}
